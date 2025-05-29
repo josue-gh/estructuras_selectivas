00b1f0b90074061215cdb03cdd3a8a80e05cce28
josue <josue.mlpro.2014@gmail.com>
2025-05-28 22:15:38 -0500

    puse un mensaje sobre el titulo de mi programa

--- EjercicioDeSemana.cpp
+++ EjercicioDeSemana.cpp
@@ -3,32 +3,40 @@ using namespace std;
 
 int main(){
 	int numero;
-	cout<<Digite un numero del uno al siete: ; cin>>numero;
+	
+	cout<<"programa sobre el dia de la semana"<<endl;
+	
+	cout<<"Digite un numero del uno al siete: " ; 
+	cin>>numero;
+	
 	
 	if(numero==1){
-		cout<<el dia de la semana es: Domingo<<endl;
+		cout<<"el dia de la semana es: Domingo"<<endl;
 	}
 	if(numero==2){
-		cout<<El dia de la semana es: Lunes<<endl;
+		cout<<"El dia de la semana es: Lunes"<<endl;
 	}
 	if(numero==3){
-		cout<<El dia de la semana es: Martes<<endl;
+		cout<<"El dia de la semana es: Martes"<<endl;
 	}
 	if(numero==4){
-		cout<<El dia de la semana es: Miercoles<<endl;
+		cout<<"El dia de la semana es: Miercoles"<<endl;
 	}
 	if(numero==5){
-		cout<<El dia de la semana es: Jueves<<endl;
+		cout<<"El dia de la semana es: Jueves"<<endl;
 	}
 	if(numero==6){
-		cout<<El dia de la semana es: Viernes<<endl;
+		cout<<"El dia de la semana es: Viernes"<<endl;
 	}
 	if(numero==7){
-		cout<<El dia de la semana es: Sabado<<endl;
+		cout<<"El dia de la semana es: Sabado"<<endl;
 	}
 	if(numero<1 || numero>7){
-		cout<<Dia de la semana no encontrado<<endl;
+		cout<<"Dia de la semana no encontrado"<<endl;
 	}
 	
+	cout<<"gracias por su participacion"<<endl;
+	
 	return 0;
+	
 }
