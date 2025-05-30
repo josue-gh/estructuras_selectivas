974ba420451d3e2183b176f85714c248224bebe9
josue <josue.mlpro.2014@gmail.com>
2025-05-29 22:29:22 -0500

    Escribi el procentaje de descuento y separe datos

--- Descuento.cpp
+++ Descuento.cpp
@@ -4,21 +4,25 @@ int main()
 {
 	float n, t, d, i, j; 
 	
-	cout<<ingrese el total de prendas compradas ;
+	cout<<"ingrese el total de prendas compradas"<<endl ;
 	cin>> n;
 	
-	cout<<ingrese el monto a pagar ;
+	cout<<"ingrese el monto a pagar"<<endl ;
 	cin>> t;
 	
 	if(n>=3){
 		i=t*20/100;
 		d=t-i;
+		
+		cout<<"el descuento a realizar es del: "<< i <<"%"<<endl;
 	}else{
 		j=t*10/100;
 		d=t-j;
+		
+		cout<<"el descuento a realizar es del: "<< j<<"%"<<endl;
 	}
 	
-	cout<<el monto a pagar final es: << d;
+	cout<<"el monto a pagar final es: " << d <<"soles";
 	
 	return 0;
 
