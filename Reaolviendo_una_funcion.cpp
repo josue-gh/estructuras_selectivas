503d3cc5f1e4f396d5bfccfa2da299c70122fe18
josue <josue.mlpro.2014@gmail.com>
2025-05-30 21:38:55 -0500

    Este en mi tercer archivo de selectivas

---
+++ Reaolviendo_una_funcion.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int x,p;
+	
+	cout<<ingrese un numero<<endl;
+	cin>> x;
+	
+	if(x>8){
+		
+		p=2*x;
+		cout<< p;
+	}else{
+		if(x>0 and x<=8){
+			p=x-5;
+			cout<<p;
+		}
+	}
+}
